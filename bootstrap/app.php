460143a34768f44901acfb9616104b09b5b0edd6
MohamedHanafiAlaoui <hanafimohamed510@gmail.com>
2025-02-25 15:29:30 +0100

    active redricte page proprietaire

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -13,6 +13,7 @@ return Application::configure(basePath: dirname(__DIR__))
     ->withMiddleware(function (Middleware $middleware) {
         $middleware->alias([
             'admin' => \App\Http\Middleware\Admin::class,
+            'proprietaire' => \App\Http\Middleware\Proprietaire::class,
         ]);
     })
     ->withExceptions(function (Exceptions $exceptions) {
